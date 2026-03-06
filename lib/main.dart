b773a40c4c58de31b4e646c465fe3b2acb3006d7
rukshantharindu582 <rukshantharindu582@gmail.com>
2026-03-06 05:17:16 +0000

    feat: Wrap website in webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:myev/webview_screen.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
@@ -22,28 +22,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('WebView App'),
-        ),
-        body: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri("https://my-flutter-app-425816.web.app"),
-          ),
-          initialSettings: InAppWebViewSettings(
-            javaScriptEnabled: true,
-            mediaPlaybackRequiresUserGesture: false,
-            allowsInlineMediaPlayback: true,
-          ),
-          onPermissionRequest: (controller, request) async {
-            return PermissionResponse(
-              resources: request.resources,
-              action: PermissionResponseAction.GRANT,
-            );
-          },
-        ),
-      ),
+    return const MaterialApp(
+      home: WebViewScreen(),
     );
   }
 }
