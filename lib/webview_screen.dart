e27ae71763d6c685692748c0a58d9b0e2b031e12
rukshantharindu582 <rukshantharindu582@gmail.com>
2026-03-06 05:34:36 +0000

    fix: Resolve webview permission errors and test issues

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({super.key});
